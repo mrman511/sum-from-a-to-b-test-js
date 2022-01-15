47f2e8d026d5552849783130303e80ef9d7e6586
Paul Bodner <bodner80@gmail.com>
2022-01-14 19:33:03 -0800

    added sumAToB recursion

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+function sum(a, b) {
+  console.log(a)
+  if(a === b) {
+    return a;
+  }
+  return a + sum(a + 1, b);
 }
-
+sum(4, 6);
 module.exports = sum;
